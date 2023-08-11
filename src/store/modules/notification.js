0cc94afa9c697541e1c2a62723d1221de78c3234
Risman04 <risman4205@gmail.com>
2023-08-11 14:36:29 +0700

    Notification

---
+++ src/store/modules/notification.js
@@ -0,0 +1,20 @@
+const notification  = {
+    namespaced: true,
+    state: {
+        notification: null,
+    },
+    mutations: {
+        SET_NOTIFICATION(state, {message, type}) {
+            state.notification = {message, type};
+        },
+    },
+    actions: {
+        setNotification({commit}, {message, type}) {
+            commit("SET_NOTIFICATION", {message, type});
+            setTimeout(() => {
+                commit("SET_NOTIFICATION", null);
+            }, 2500);
+        },
+    },
+};
+export default notification;
--- src/store/modules/users.js
+++ src/store/modules/users.js
@@ -20,10 +20,26 @@ const users = {
                 console.log(error);
             }
         },
+        async createUser({ commit, userData }) {
+            try {
+                const response = await axios.get(
+                    "https://fakestoreapi.com/users",
+                    userData
+                );
+                commit("ADD_USER", response.data);
+                return response.data;
+            } catch (error) {
+                alert(error);
+                console.log(error);
+            }
+        },
     },
     mutations: {
         SET_USERS(state, users) {
             state.userData = users;
+        },
+        SET_USER(state, user) {
+            state.userData.push(user);
         },
     },
 };
